f726bb9ac490f8c61e40ca4030ef43272d6b2015
programmersh12 <leoshev666@gmail.com>
2025-05-14 13:09:11 +0200

    2
    2

--- phone_store/shop/admin.py
+++ phone_store/shop/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Phone, Category
 
-# Register your models here.
+admin.site.register(Phone)
+admin.site.register(Category)
--- phone_store/shop/models.py
+++ phone_store/shop/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Phone(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='phones/')
+
+    def __str__(self):
+        return self.name
