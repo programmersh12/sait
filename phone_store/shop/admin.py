201fb62b08d6e092ad5e6673d2f1f7951b39e22b
maksoon. <leoshev666@gmail.com>
2025-06-16 18:07:35 +0500

    223333
    22333333

--- phone_store/shop/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Phone, Category
-
-admin.site.register(Phone)
-admin.site.register(Category)
--- phone_store/shop/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import Phone
-# Create your views here.
-def home(request):
-    phones = Phone.objects.all()
-    return render(request, 'shop/home.html', {'phones': phones})
